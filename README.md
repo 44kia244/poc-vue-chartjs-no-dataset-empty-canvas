acd5297b15511d7667393ea3b0fcf95eb3686149
44kia244 <kiarttipum@hotmail.com>
2022-12-14 11:20:35 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# poc-vue-chartjs-no-dataset-empty-canvas
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-t9rqff)
---
+++ src/chartConfig.ts
@@ -0,0 +1,22 @@
+export const data = {
+  labels: [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ],
+  datasets: []
+}
+
+export const options = {
+  responsive: true,
+  maintainAspectRatio: false
+}
